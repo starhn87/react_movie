f64c9c7f1cb230233193022c14f339029e6ee435
이승우 <starhn87@gmail.com>
2020-07-15 15:02:51 +0900

    Updates

---
+++ precache-manifest.49dce364a8cc58cdca2260f13e5cd4a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52d9a1c580d5f0331372cbc9853c706",
+    "url": "/react_movie/index.html"
+  },
+  {
+    "revision": "27c39aeb9c0b82d850b9",
+    "url": "/react_movie/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "d9a935b21f9efabafb32",
+    "url": "/react_movie/static/js/2.3976b992.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_movie/static/js/2.3976b992.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27c39aeb9c0b82d850b9",
+    "url": "/react_movie/static/js/main.47bf0375.chunk.js"
+  },
+  {
+    "revision": "cb9354ec2cd5d0f2078a",
+    "url": "/react_movie/static/js/runtime-main.ba1d8f8d.js"
+  }
+]);
